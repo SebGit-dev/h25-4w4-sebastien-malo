3aa9deb4e53679f23e6ee1770e00c97a8eac7b0c
SebastienGithub <hikup.github@gmail.com>
2025-03-12 15:50:29 -0400

    s8c1 - Function.php est maintenant separe avec customizer.php et options.php

--- functions.php
+++ functions.php
@@ -1,75 +1,16 @@
 <?php
-function theme_tp_customize_register($wp_customize){
-  $wp_customize->add_section('hero_section', array(
-    'title' => __('Hero Section', 'theme_tp'),
-    'priority' => 30,
-  ));
+// Chemin vers le dossier functions
+$functions_dir = get_template_directory() . '/functions/';
 
-  $wp_customize->add_setting('hero_auteur', array(
-    'default' => __('Sebastien Malo', 'theme_tp'),
-    'sanitize_callback' => 'sanitize_text_field'
-  ));
+// Liste des fichiers à inclure
+$function_files = array(
+    'customizer.php',
+    'options.php'
+);
 
-  $wp_customize->add_control('hero_auteur', array(
-    'label' => __('Auteur', 'theme_tp'),
-    'section' => 'hero_section',
-    'type' => 'text',
-  ));
-
-  $wp_customize->add_setting('hero_background', array(
-    'default' => '',
-    'sanitize_callback' => 'esc_url_raw',
-  ));
-
-  $wp_customize->add_control(new WP_Customize_Image_Control($wp_customize, 'hero_background', array(
-      'label' => __('Image en arriere plan', 'theme_tp'),
-      'section' => 'hero_section',
-  )));
+// Boucle pour inclure tous les fichiers
+foreach ($function_files as $file) {
+    include_once $functions_dir . $file;
 }
-
-add_action('customize_register', 'theme_tp_customize_register');
-
-function mon_theme_supports() {
-
-  add_theme_support('title-tag');
-  add_theme_support('menus');
-  add_theme_support('post-thumbnail');
-  add_theme_support('custom-logo', array(
-    'height'      => 250,
-    'width'       => 250,
-    'flex-height' => true,
-    'flex-width'  => true,
-));
-
-}
-add_action( 'after_setup_theme', 'mon_theme_supports' );
-
-
-function theme_4w4_enqueue_styles() { 
-wp_enqueue_style('normalize', get_template_directory_uri() . '/normalize.css');  
-wp_enqueue_style('mon-style-style', get_stylesheet_uri()); 
-} 
-/* 
-*/
-add_action('wp_enqueue_scripts', 'theme_4w4_enqueue_styles');
-
-/**
- * Modifie la requete principale de WordPress avant qu'elle soit exécuté
- * le hook « pre_get_posts » se manifeste juste avant d'exécuter la requête principal
- * Dépendant de la condition initiale on peut filtrer un type particulier de requête
- * Dans ce cas ci nous filtrons la requête de la page d'accueil
- * @param WP_query  $query la requête principal de WP
- */
-
-
-function modifie_requete_principal( $query ) {
-    if ( $query->is_home() && $query->is_main_query() && ! is_admin() ) {
-      $query->set( 'category_name', 'populaire' );
-      $query->set( 'orderby', 'title' );
-      $query->set( 'order', 'ASC' );
-      }
-     }
-     add_action( 'pre_get_posts', 'modifie_requete_principal' );
-
 
 ?>
