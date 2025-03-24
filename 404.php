56ab52552f209dff164608df27713e12a3af9135
SebastienGithub <hikup.github@gmail.com>
2025-03-24 15:57:36 -0400

    s9c1 - Ajout de l'image fait dans photoshop pour la page 404

--- 404.php
+++ 404.php
@@ -1,4 +1,7 @@
 <?php get_header(); ?>
     <!-- <h1>-------- 404.PHP ----------</h1> -->
-    <div class="error">Error 404</div>
+    <figure class="error__img">
+        <!-- J'ai fait l'image dans Photoshop -->
+        <img src="https://gftnth00.mywhc.ca/4w4_18/wp-content/uploads/2025/03/404-travel-2.jpg-4.png" alt="404">
+    </figure>
 <?php get_footer(); ?>
