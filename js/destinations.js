751b9e8bb9162f3be12882da0d8c85fb9523ab19
Malo <e2129395@cmaisonneuve.qc.ca>
2025-04-02 17:58:19 -0400

    s10c2 - save dans fonction prendreDestination

--- js/destinations.js
+++ js/destinations.js
@@ -6,26 +6,27 @@
 function parcourir_boutons(){
     const categorie_ul__li = document.querySelectorAll(".categorie_ul__li")
     categorie_ul__li.forEach(elm => {
-        elm.addEventListener("click", prendreDestination())
+        elm.addEventListener("click", prendreDestination);
     })
 }
-git 
+
 function prendreDestination(){
     fetch(apiUrl)
-        .then(response => response.json())
-        .then(data => {
-            const destinationList = document.querySelector('.destination__list');
-            data.forEach(article => {
-                const articleElement = document.createElement('div');
-                // <div>${article.excerpt.rendered}</div>
-                articleElement.innerHTML = `
-                    <h3>${article.title.rendered}</h3>
-                    <a href="${article.link}">Lire plus</a>
-                `;
-                destinationList .appendChild(articleElement);
-            });
-        })
-        .catch(error => console.error('Erreur lors de la récupération des articles:', error));
+    .then(response => response.json())
+    .then(data => {
+        const destinationList = document.querySelector('.destination__list');
+        destinationList.innerHTML = "";
+        data.forEach(article => {
+            const articleElement = document.createElement('div');
+            // <div>${article.excerpt.rendered}</div>
+            articleElement.innerHTML = `
+                <h3>${article.title.rendered}</h3>
+                <a href="${article.link}">Lire plus</a>
+            `;
+            destinationList .appendChild(articleElement);
+        });
+    })
+    .catch(error => console.error('Erreur lors de la récupération des articles:', error));
     }
 })()
 
