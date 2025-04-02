3aa034568facb8215051e962cbd38fa574a1e71c
Malo <e2129395@cmaisonneuve.qc.ca>
2025-04-02 17:57:01 -0400

    s10c2 - Ajout du fichier destination.js et ajout fonction prendreDestination

--- front-page.php
+++ front-page.php
@@ -12,6 +12,12 @@
             <?php endwhile; endif; ?>
         </div>
     </section>
+    <!---------- Section rest ap ------------>
+    <section class="destination">
+        <?php categories_liste("destination") ?>
+        <h2 class="destination__titre">Articles de la catégorie</h2>
+        <div class="destination__list"></div>
+    </section>
     <?php get_footer(); ?>
 </body>
 </html>
---
+++ js/destinations.js
@@ -0,0 +1,33 @@
+(function(){
+    const categoryId = 3; // Remplacez par l'ID de la catégorie souhaitée
+    const domaine = window.location.href;
+    const apiUrl = `${domaine}wp-json/wp/v2/posts?categories=${categoryId}`;
+    console.log("destinationj.js");
+function parcourir_boutons(){
+    const categorie_ul__li = document.querySelectorAll(".categorie_ul__li")
+    categorie_ul__li.forEach(elm => {
+        elm.addEventListener("click", prendreDestination())
+    })
+}
+git 
+function prendreDestination(){
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            const destinationList = document.querySelector('.destination__list');
+            data.forEach(article => {
+                const articleElement = document.createElement('div');
+                // <div>${article.excerpt.rendered}</div>
+                articleElement.innerHTML = `
+                    <h3>${article.title.rendered}</h3>
+                    <a href="${article.link}">Lire plus</a>
+                `;
+                destinationList .appendChild(articleElement);
+            });
+        })
+        .catch(error => console.error('Erreur lors de la récupération des articles:', error));
+    }
+})()
+
+
+    
