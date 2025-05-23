8ae38c44a4d67d8c275f3de9ce1dcf70337a298d
SebastienGithub <hikup.github@gmail.com>
2025-05-23 09:32:29 -0400

    ef-SM  ajout de la page scss pays

--- template-pays.php
+++ template-pays.php
@@ -7,30 +7,34 @@ Template Name: Template Pays
 get_header();
 ?>
 
-<section class="populaire">
-    <div class="global">
-        <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-                <article>
-                    <h2><?php the_title(); ?></h2>
-                    <div><?php the_content(); ?></div>
-                </article>
-        <?php endwhile;
-        endif; ?>
-    </div>
-</section>
+<div class="page_pays">
+    <section class="populaire">
+        <div class="global">
+            <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+                    <article>
+                        <h2><?php the_title(); ?></h2>
+                        <div><?php the_content(); ?></div>
+                    </article>
+            <?php endwhile;
+            endif; ?>
+        </div>
+    </section>
 
-<?php 
-genere_vague($rest_api_wave_color);
-?>
- 
-<!-- Rest API -->
-<section class="destination__pays">
-    <?php categories_liste("destination"); ?>
-    <h2 class="destination__pays__titre">Destinations du Pays</h2>
-    <div class="destination__pays__list"></div>
-</section>
+    <?php 
+    genere_vague($rest_api_wave_color);
+    ?>
+    
+    <!-- Rest API -->
+    <section class="destination__pays">
+        <?php categories_liste("destination"); ?>
+        <h2 class="destination__pays__titre">Destinations du Pays</h2>
+        <div class="destination__pays__list"></div>
+    </section>
 
-<?php get_footer(); ?>
+    <?php get_footer(); ?>
+</div>
+
+
 
 
  
