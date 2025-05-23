bf6f691584bfb8d73172363e7bda236c14aea090
SebastienGithub <hikup.github@gmail.com>
2025-05-23 08:41:28 -0400

    Examen Final - ajout de la page template-pays.php, ajout de la section populaire, preparations a ajouter la partie de rest api

--- js/destination.js
+++ js/destination.js
@@ -34,5 +34,55 @@ function getDestination(event) {
         .catch(error => console.error('Erreur lors de la récupération des articles:', error));
 }
 
+(function () {
+    const domaine = window.origin + "/4w4_18/";
+
+    (function parcourir_bouton() {
+        const categorie__ul__li = document.querySelectorAll(".categorie__ul__li");
+        categorie__ul__li.forEach(elm => {
+            elm.addEventListener("click", afficherDestination);
+        })
+    })()
+
+    async function afficherDestination(event) {
+        const categoryId = event.target.getAttribute('data-categoryID'); // Remplacez par l'ID de la catégorie souhaitée
+        const apiUrl = `${domaine}wp-json/wp/v2/posts?categories=${categoryId}`;
+        console.log(domaine);
+        fetch(apiUrl)
+            .then(response => response.json())
+            .then(data => {
+                const destinationList = document.querySelector('.destination__list');
+                // console.log(`data = ` + data);
+                destinationList.innerHTML = "";
+                data.forEach(article => {
+                    const articleElement = document.createElement('div');
+                    articleElement.classList.add("destination__list__article")
+                    console.log(article.title.rendered);
+
+                    articleElement.innerHTML = `
+                    <h3>${article.title.rendered}</h3>
+                    <button class="bntArticle"><img src=""></button>
+                    <section>
+                        ${article.excerpt.rendered}
+                        <a href="${article.link}">Lire plus</a>
+                    </section>
+                `;
+                    destinationList.appendChild(articleElement);
+                });
+                let deroulant = document.querySelectorAll('.destination__list__article');
+                deroulant.forEach(elm => {
+                    elm.addEventListener("click", afficherTexte);
+                })
+            })
+            .catch(error => console.error('Erreur lors de la récupération des articles:', error));
+    }
+
+    function afficherTexte(event) {
+        let description = event.target.closest("div").querySelector("section");
+        event.target.closest("button").classList.toggle("ouvert");
+        description.classList.toggle("affichage");
+    }
+}
+)()
 
     
---
+++ template-pays.php
@@ -0,0 +1,30 @@
+<?php
+/*
+Template Name: Template Pays
+*/
+get_header();
+?>
+
+<section class="populaire">
+    <div class="global">
+        <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+                <article>
+                    <h2><?php the_title(); ?></h2>
+                    <div><?php the_content(); ?></div>
+                </article>
+        <?php endwhile;
+        endif; ?>
+    </div>
+</section>
+ 
+<!-- Rest API -->
+<section class="destination__pays">
+    <?php categories_liste("destination"); ?>
+    <h2 class="destination__pays__titre">Destinations du Pays</h2>
+    <div class="destination__pays__list"></div>
+</section>
+<?php get_footer(); ?>
+
+
+ 
+
